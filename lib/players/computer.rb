457a1ddac3094b8ac6ba2895715d962100459424
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-31 14:51:48 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,27 +1,17 @@
 module Players
   class Computer < Player
 
-    WIN_COMBINATIONS = [
-      [0,1,2],  # Top row
-      [3,4,5],  # Middle row
-      [6,7,8],  # Bottom rom
-      #-----
-      [0,3,6],  # Col. 1
-      [1,4,7],  # Col. 2
-      [2,5,8],  # Col. 3
-      #-----
-      [0,4,8],  # L to R diagonal
-      [2,4,6]   # R to L diagonal
-    ]
-
-    CORNERS = [0,2,6,8]
-
     def move(board)
       if board.position("5") == " "
         "5"
-
-
-
+      elsif board.position("0") == " "
+        "0"
+      elsif board.position("2") == " "
+        "2"
+      elsif board.position("6") == " "
+        "6"
+      elsif board.position("8") == " "
+        "8"
 
       # else board.cells.each_with_index do |cell, index|
       #   if cell == " "
