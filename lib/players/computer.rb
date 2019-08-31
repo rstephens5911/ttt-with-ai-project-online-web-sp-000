fff40161f632fa658d5a16fca2cec29d6b7869d2
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-31 15:00:05 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,20 +4,23 @@ module Players
     def move(board)
       if board.position("5") == " "
         return "5"
-      elsif board.position("1") == " "
-          return "0"
-      elsif board.position("3") == " "
-        return "3"
-      elsif board.position("7") == " "
-          return "7"
-      elsif board.position("9") == " "
-          return "9"
+      else
+        case 
+        when board.taken?(1) && !board.take?(9)
+          9
+        when board.taken?(9) && !board.taken?(1)
+          1
+        when board.taken?(7) && !board.taken(3)
+          3
+        when board.taken?(3) && !board.taken(7)
+          7
+        end
       end
     end
   end
 end
-
       # else board.cells.each_with_index do |cell, index|
       #   if cell == " "
       #     sleep(1)
+
       #     return index + 1
